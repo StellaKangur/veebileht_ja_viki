71aae0058f8e82d2cfeb8dc4dfa2111d84a9ddde
StellaKangur <44642873+StellaKangur@users.noreply.github.com>
2018-11-14 17:03:28 +0200

    Delete REEEEEEEEEEEEEEE.py

--- REEEEEEEEEEEEEEE.py
+++
@@ -1,9 +0,0 @@
-mehu = 90
-koda = 20
-aeg = 0
-
-while mehu>=20.1:
-    jahemehu = (mehu - koda) * 0.19
-    mehu = mehu-jahemehu
-    aeg+=1
-    print("Supi temperatuur",aeg,". minutil on",round(mehu,2),"kraadi.")
